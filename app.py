6c827c38c1dcb0981b32d52fa2b886a823bda660
DongDong <jjes0107@gmail.com>
2026-02-07 06:29:30 +0000

    중간 저장

--- app.py
+++ app.py
@@ -10,32 +10,13 @@ st.set_page_config(
 
 # 2. 메뉴바 설정(각 페이지의 실제 콘텐츠는 별도의 파일에 존재).
 pages = {
-    "중1 수학": [
-        # 그룹의 첫 번째 페이지를 default=True로 설정하면 '중1 수학' 클릭 시 이 페이지가 먼저 보입니다.
-        st.Page("rotation.py", title="회전체 탐구", default=True),
-    ],
-    "중2 수학": [
-        st.Page("./중2 수학/Exponents.py", title="지수법칙"),
-    ],
-    "보드게임": [
-        st.Page("./보드게임/Streams.py", title="스트림스"),
-        st.Page("./보드게임/PigGame.py", title="Pig Game"),
-    ],
-    "주사위 모음": [
-        st.Page("Dice.py", title="주사위 모음")
-    ],
     "인공지능 수학": [
-        st.Page("./인공지능수학/ImageExpression.py", title="이미지 데이터의 표현"),
-        st.Page("./인공지능수학/ImageConversion.py", title="이미지 데이터의 변환"),
+        st.Page("./인공지능수학/Dissolve.py", title="디졸브 효과"),
     ],
-    "산업수학": [
-        st.Page("./산업수학/MedicalData.py", title="의료 데이터와 건강 상태"),
-        st.Page("./산업수학/DistanceOptimization.py", title="원자력 발전소 기중기의 이동 경로 최적화"),
-    ],    
 }
 
 # 3. 네비게이션 UI 생성(메뉴바 위치)
-pg = st.navigation(pages, position="top")
+pg = st.navigation(pages, position="hidden")
 
 # 4. 사용자가 선택한 페이지 실행
 pg.run()
